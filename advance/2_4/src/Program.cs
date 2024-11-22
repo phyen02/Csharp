848c1ba6aa0ba84c176380abbd1f3f76d0572379
Pham Hoang Yen <76261980+phyen02@users.noreply.github.com>
2024-11-23 01:47:54 +0700

    test practice

---
+++ advance/2_4/src/Program.cs
@@ -0,0 +1,21 @@
+﻿/* STUDENT MANAGEMENT */
+
+namespace System
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Student
+    {
+        // id, tên, giới tính, tuổi, điểm toán, điểm lý, điểm hóa, điểm trung bình và học lực.
+        private int SID;
+        private char gender;
+        private double pMath, pPhy, pChe;
+        private string performance = "";
+    }
+}
---
+++ practice/practice/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace System
+{
+    public class Kata
+    {
+        public static int FindSmallestInt(int[] args)
+        {
+            int min = 0;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if(args[i] < min)
+                    min = args[i];
+            }
+            return min;
+        }
+
+        public static void Main(string[] args)
+        {
+            //int[] arr1 = new int[4] {34, 15, 88, 2};
+            int[] arr2 = new int[4] {34, -345, -1, 100};
+            //Console.WriteLine(FindSmallestInt(arr1));
+            Console.WriteLine(FindSmallestInt(arr2));
+        }
+    }
+}
