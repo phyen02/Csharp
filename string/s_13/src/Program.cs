c4429af679e11b60c2e1c96009efa75a8f742579
Pham Hoang Yen <76261980+phyen02@users.noreply.github.com>
2024-12-23 17:22:21 +0700

    update completed practice

---
+++ string/s_13/src/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Xoá các nguyên âm trong chuỗi có sẵn, trả về chuỗi đã xoá nguyên âm
+Ví dụ: "This website is for losers LOL!" → "Ths wbst s fr lsrs LL!”
+*/
+using System.Text.RegularExpressions;
+
+namespace Program
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string comment = "This website is for losers LOL!";
+            Console.WriteLine(comment.RemoveVowel());
+        }
+
+        public static string RemoveVowel(this string comment)
+        {
+            return Regex.Replace(comment ,"[aeiou]", "", RegexOptions.IgnoreCase);
+        }
+    }
+}
---
+++ string/s_14/src/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Đếm số lượng các từ nguyên âm có trong chuỗi
+*/
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CountVowel("asahduaiasd"));
+        }
+
+        public static int CountVowel(string str)
+        {
+            return str.ToLower().Count(c => "aeiou".IndexOf(c) != -1);
+        }
+    }
+}
